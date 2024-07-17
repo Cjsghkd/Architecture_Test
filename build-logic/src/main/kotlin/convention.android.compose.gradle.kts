86428c4211f20ecf0527239d8c2e945fae6c2234
sunghyun <pjhomg5530@naver.com>
2024-07-17 10:09:25 +0900

    :sparkles: convention.android.compose 생성

---
+++ build-logic/src/main/kotlin/convention.android.compose.gradle.kts
@@ -0,0 +1,6 @@
+import com.example.architecture_test.configureComposeAndroid
+import gradle.kotlin.dsl.accessors._34dc1868ea961489243ddc5bd8557647.android
+
+android {
+    configureComposeAndroid(this)
+}
