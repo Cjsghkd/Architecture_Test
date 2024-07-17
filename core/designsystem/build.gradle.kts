a28e056e1de351a656e32a33739824bc3a239c81
sunghyun <pjhomg5530@naver.com>
2024-07-17 10:09:32 +0900

    :sparkles: convention.android.compose 적용

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,31 +1,15 @@
 plugins {
     id("convention.android.library")
+    id("convention.android.compose")
 }
 
 android {
     namespace = "com.example.designsystem"
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
-    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-
-    val composeBom = platform(libs.androidx.compose.bom)
-    implementation(composeBom)
-
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext)
